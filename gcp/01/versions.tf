7e9a0a658891592b8829b343a683386f50a6374d
gordonpm <gordonm0815@gmail.com>
2026-08-02 18:10:18 -0700

    added TF scripts for GCP

---
+++ gcp/01/versions.tf
@@ -0,0 +1,18 @@
+# Terraform Settings Block
+terraform {
+  required_version = "~> 1.15.8"
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 7.42.0"
+    }
+  }
+}
+
+# Terraform Provider Block
+provider "google" {
+  project = "experiments-502900" # PROJECT_ID
+  region = "us-west1"
+  zone = "us-west1-c"
+}
+
---
+++ gcp/01/vm-instance.tf
@@ -0,0 +1,21 @@
+# Resource Block: Create a single Compute Engine instance
+resource "google_compute_instance" "myapp1" {
+  name         = "vm-pc-exp1"
+  machine_type = "e2-micro"
+  tags        = [tolist(google_compute_firewall.fw_ssh.target_tags)[0], tolist(google_compute_firewall.fw_http.target_tags)[0]]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-13"
+      type = "pd-standard"
+      size = 20
+    }
+  }
+  # Install Webserver
+  metadata_startup_script = file("${path.module}/app1-webserver-install.sh")
+  network_interface {
+    subnetwork = google_compute_subnetwork.mysubnet.id 
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
